861dbeed83aa0556d0dd2ea7aea30f07e0d1ce6e
Prashant Shukla <prashantshukla766@gmail.com>
2024-09-07 19:56:03 +0530

    Add auth middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.token || req.header('Authorization')?.replace("Bearer ", "").trim(); // Trim whitespace
+    console.log(token);
+
+    if (!token) {
+        throw new ApiError(400, "Unauthorized, Invalid Token");
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET); // Ensure the token is validated with the secret key
+        console.log(decodedToken);
+
+        req.user = decodedToken._id; 
+        next();
+    } catch (error) {
+        console.error('Token verification error:', error);
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized, Invalid Token"
+        });
+    }
+});
+
+
+export {
+    verifyJWT
+}
