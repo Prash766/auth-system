c6311bf3073d9c5a10b860d6c3e4b198b40a4eea
Prashant Shukla <prashantshukla766@gmail.com>
2024-09-07 15:13:25 +0530

    feat(utils): Add custom error class and error handling middleware

--- backend/utils/ApiError.js
+++ backend/utils/ApiError.js
@@ -1,11 +1,21 @@
 class ApiError extends Error {
     constructor(
         status,
-        message="Something went wrong"
+        message="Something went wrong",
+        errors=[],
+        stack=""
     ){
         super(message)
         this.status = status
         this.data = null
+        this.success = false
+        this.errors = errors
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this , this.constructor)
+        }
     }
 
 }
