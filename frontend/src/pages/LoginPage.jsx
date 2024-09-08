ab57bdbe06c625357242a1f8dc5039fc989e9a01
Prashant Shukla <prashantshukla766@gmail.com>
2024-09-08 21:42:19 +0530

    Implement Login and Signup pages for authentication

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
+import { Link, useNavigate } from 'react-router-dom'
+import { Loader } from 'lucide-react'
+import { useRecoilValue } from 'recoil'
+import { Loading, useAuthActions } from '../../atoms/Auth'
+import { toast } from 'sonner'
 
 const LoginPage = () => {
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+  const isLoading = useRecoilValue(Loading)
+  const {login} = useAuthActions()
+  const navigate = useNavigate()
+
+async function handleLogin(e){
+  e.preventDefault()
+  try {
+    const response = await login(email ,password)
+    console.log(response)
+    if(response.status===200){
+      toast.success("Logged in Successfully")
+      navigate('/' ,{replace:true})
+      
+    }
+  } catch (error) {
+    console.error(error)
+    
+  }
+
+}
   return (
     <div className="flex  items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-violet-800">
       <motion.div
@@ -29,6 +56,8 @@ const LoginPage = () => {
           </label>
           <input
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="email"
             className="w-full mb-4 px-3 py-2 bg-white bg-opacity-20 border border-gray-300 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition duration-200"
             placeholder="you@example.com"
@@ -46,6 +75,8 @@ const LoginPage = () => {
           </label>
           <input
             type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
             id="password"
             className="w-full px-3 mb-4 py-2 bg-white bg-opacity-20 border border-gray-300 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition duration-200"
             placeholder="••••••••"
@@ -61,28 +92,28 @@ const LoginPage = () => {
         >
     
           <div className="text-sm">
-            <a href="#" className="font-medium text-purple-300 hover:text-purple-200">
+            <Link to='/forgot-password' className="font-medium text-purple-300 hover:text-purple-200">
               Forgot your password?
-            </a>
+            </Link>
           </div>
         </motion.div>
 
         <motion.button
           type="submit"
-          
+          onClick={(e)=>handleLogin(e)}
           className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:ring-offset-2 focus:ring-purple-500 "
           whileHover={{ scale: 1.1,
             transition: { duration: 0.5 },
           }}
           whileTap={{ scale: 0.8,
             transition: { duration: 0.5 },
-
            }}
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{delay:0.4 , duration:0.6}}
+          disabled={isLoading}
         >
-          Sign in
+        {isLoading? <Loader className='w-6 h-6 animate-spin mx-auto '/>:"Login"}
         </motion.button>
 
         <motion.p
@@ -92,9 +123,9 @@ const LoginPage = () => {
           transition={{ delay: 0.7, duration: 0.5 }}
         >
           Not a member?{' '}
-          <span className="underline cursor-pointer  font-medium text-purple-300 hover:text-purple-200">
+          <Link to='/signup' className="underline cursor-pointer  font-medium text-purple-300 hover:text-purple-200">
             Sign up now
-          </span>
+          </Link>
         </motion.p>
       </motion.div>
     </div>
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { Mail } from 'lucide-react'
+import { Loader, Mail } from 'lucide-react'
 import PasswordStrengthMeter from '../components/PasswordStrength'
+import { Link, useNavigate } from 'react-router-dom'
+import { Loading, useAuthActions } from '../../atoms/Auth'
+import { useRecoilValue } from 'recoil'
 
 const SignupPage = () => {
 
   const [fullName, setFullName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+const {signup } = useAuthActions()
+const isLoading = useRecoilValue(Loading)
+
+async function handleSignUp(){
+try {
+   const response = await signup(email , password , fullName)
+   console.log(response)
+   if(response.status ===200){
+     navigate('/verify-email')
+   }
+} catch (error) {
+  console.error(error)
+}
+}
 
 
   return (
@@ -16,7 +34,7 @@ const SignupPage = () => {
         className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-xl p-8 w-full max-w-md"
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
-        transition={{ delay: 0.4, duration: 0.5 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
       >
         <motion.h2
           className="text-3xl font-bold text-white mb-6 text-center"
@@ -29,7 +47,7 @@ const SignupPage = () => {
         <motion.div
           initial={{ x: -20, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.8 }}
+          transition={{ delay: 0.4, duration: 0.8 }}
           className='space-y-4'
         >
           <motion.div
@@ -94,9 +112,11 @@ const SignupPage = () => {
           <PasswordStrengthMeter password={password} />
           <motion.button
             whileTap={{ scale: 0.8 }}
+            onClick={handleSignUp}
             className='bg-purple-600 w-full rounded-md p-2 font-semibold text-white hover:bg-purple-700'
           >
-            Sign Up
+            {isLoading ? <Loader className='w-6 h-6 animate-spin mx-auto ' /> : "Sign Up"}
+
           </motion.button>
 
           <motion.p
@@ -106,9 +126,9 @@ const SignupPage = () => {
             transition={{ delay: 0.7, duration: 0.5 }}
           >
             Already a Member?{' '}
-            <span className="underline cursor-pointer font-medium text-purple-300 hover:text-purple-200">
+            <Link to='/login' className="underline cursor-pointer font-medium text-purple-300 hover:text-purple-200">
               Login now
-            </span>
+            </Link>
           </motion.p>
         </motion.div>
       </motion.div>
